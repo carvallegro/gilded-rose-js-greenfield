ed667b33f37a23ab2650d26c987677f16d7de653
Carvallegro <valentin.carmignac@gmail.com>
2018-05-29 14:40:12 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const _ = require('lodash')
+
+const MIN_QUALITY = 0
+const MAX_QUALITY = 50
+
+const updateItems = (items = []) => {
+    const splitBySulfuras = _.partition(items, { name: 'Sulfuras' })
+    const sulfurasItems = splitBySulfuras[0]
+
+    const itemsWithUpdatedSellIn = _(splitBySulfuras[1])
+        .flatMap(updateSellIn)
+        .value();
+
+    const splitByAgedBrie = _.partition(itemsWithUpdatedSellIn, { name: 'Aged Brie' })
+
+    const updatedAgedBries = _.flatMap(splitByAgedBrie[0], updateBrieQuality)
+
+    const updatedStandard = _(splitByAgedBrie[1])
+        .flatMap(updateStandardQuality)
+        .value()
+    return [
+        ...sulfurasItems,
+        ...updatedAgedBries,
+        ...updatedStandard
+    ]
+}
+
+const updateSellIn = i => ({
+    ...i,
+    sellIn: i.sellIn - 1,
+})
+
+const updateBrieQuality = i => ({
+    ...i,
+    quality: Math.min(MAX_QUALITY, getNewBrieQuality(i))
+})
+const getNewBrieQuality = i =>
+    i.sellIn > 0
+        ? i.quality + 1
+        : i.quality + 2
+
+const updateStandardQuality = i => ({
+    ...i,
+    quality: Math.max(MIN_QUALITY, getNewQuality(i))
+})
+
+const getNewQuality = i =>
+    i.sellIn > 0
+        ? i.quality - 1
+        : i.quality - 2
+
+module.exports = {
+    updateItems
+}
---
+++ index.test.js
@@ -0,0 +1,49 @@
+const store = require('./index')
+
+describe('The Gilded Rose store', () => {
+    describe('Standard objects', () => {
+        it('should modify sellIn by -1', () => {
+            const param = [{name: 'Mighty Book', sellIn: 10, quality: 10}]
+            expect(store.updateItems(param)).toHaveProperty('0.sellIn', 9)
+        })
+
+        it('should modify quality by -1', () => {
+            const param = [{name: 'Mighty Book', sellIn: 10, quality: 10}]
+            expect(store.updateItems(param)).toHaveProperty('0.quality', 9)
+        })
+
+        it('should rotten twice as much when object is expired', () => {
+            const param = [{name: 'Mighty Book', sellIn: 1, quality: 10}]
+            expect(store.updateItems(param)).toHaveProperty('0.quality', 8)
+        })
+
+        it('should never have quality below zero', () => {
+            const param = [{name: 'Mighty Book', sellIn: 1, quality: 0}]
+            expect(store.updateItems(param)).toHaveProperty('0.quality', 0)
+        })
+    })
+
+    describe('Aged Brie', () => {
+        it('should augment quality the older it gets', () => {
+            const param = [{name: 'Aged Brie', sellIn: 10, quality: 10}]
+            expect(store.updateItems(param)).toHaveProperty('0.quality', 11)
+        })
+
+        it('should augment quality twice as much when it is expired', () => {
+            const param = [{name: 'Aged Brie', sellIn: 0, quality: 10}]
+            expect(store.updateItems(param)).toHaveProperty('0.quality', 12)
+        })
+
+        it('should never have a quality above 50', () => {
+            const param = [{name: 'Aged Brie', sellIn: 0, quality: 50}]
+            expect(store.updateItems(param)).toHaveProperty('0.quality', 50)
+        })
+    })
+
+    describe('Sulfuras', () => {
+        it('should never change', () => {
+            const param = [{name: 'Sulfuras', sellIn: 10, quality: 80}]
+            expect(store.updateItems(param)).toEqual(param)
+        })
+    })
+})
